84d77f9356f1ce3b9a6dff056a7ada2ef6323d8b
chavlin <chris.havlin@gmail.com>
2026-06-25 10:34:49 -0700

    create an fill-in-the blank version of the first ex

--- VBRc_Demo/ex_00_basic_usage.m
+++ VBRc_Demo/ex_00_basic_usage.m
@@ -10,32 +10,24 @@ addpath(path_to_top_level_vbr)
 % or ~/.octaverc for octave (https://docs.octave.org/interpreter/Startup-Files.html)
 vbr_init
 
-% vbr_version
+
+
+
+
+
+
+
 % 2. initialize the VBR structure
 %    - set the properties and methods
 %    - set thermodynamic state variable arrays  (T, phi)
 
-VBR = struct();
-VBR.in = struct();
-
 vbrListMethods
 
-VBR.in.elastic.methods_list={'anharmonic';'anh_poro';};
-VBR.in.viscous.methods_list={'HK2003';'HZK2011'};
-VBR.in.anelastic.methods_list={'eburgers_psp';'andrade_psp';'xfit_mxw'; 'backstress_linear'};
 
-%  frequencies to calculate at
-VBR.in.SV.f = logspace(-2.2,-1.3,4); % [Hz]
 
-%  size of the state variable arrays. arrays can be any shape
-%  but all arrays must be the same shape.
-VBR.in.SV.T_K = linspace(800, 1400, 100)+273; % temperature [K]
-sz = size(VBR.in.SV.T_K);
-VBR.in.SV.P_GPa = 2 * ones(sz); % pressure [GPa]
-VBR.in.SV.rho = 3300 * ones(sz); % density [kg m^-3]
-VBR.in.SV.sig_MPa = 10 * ones(sz); % differential stress [MPa]
-VBR.in.SV.phi = 0.0 * ones(sz); % melt fraction
-VBR.in.SV.dg_um = 0.01 * 1e6 * ones(sz); % grain size [um]
+VBR = struct();
+
+
 
 % what SVs are required? check docs!
 % https://vbr-calc.github.io/vbr/vbrmethods/viscous/
@@ -46,47 +38,82 @@ VBR.in.SV.dg_um = 0.01 * 1e6 * ones(sz); % grain size [um]
 % 3. call the VBRc
 VBR = VBR_spine(VBR);
 
+
+
+
+
+
+
+
+
 % 4. inspect output
 % tab-complete VBR.
 % shape of arrays (frequency dependence)
 % units of output
-disp(fieldnames(VBR.out))
-disp(fieldnames(VBR.out.anelastic))
-disp(fieldnames(VBR.out.anelastic.eburgers_psp))
+
+
+
+
+
+
+
 
 % for outputs: https://vbr-calc.github.io/vbr/ left hand method types
 
 % units of outputs
-VBR.out.anelastic.eburgers_psp.units
+
+
+
+
+
+
+
+
 
 % 5. method citations
-VBR.in.anelastic.andrade_psp.citations
+
+
+
+
+
+
+
+
 %ans =
 %{
 %  [1,1] = Jackson and Faul, 2010, Phys. Earth Planet. Inter., https://doi.org/10.1016/j.pepi.2010.09.005
 %}
 
-% 6. Looping over structures to compare methods
-viscous_fields = fieldnames(VBR.out.viscous)  % cell array
-viscous_fields(1) % single element of cell array
-viscous_fields{2} % just the string
 
-nfields = numel(viscous_fields);
-figure()
-for ifield = 1:nfields
-    visc = VBR.out.viscous.(viscous_fields{ifield});
-    eta_total = visc.eta_total;
-    hold all
-    semilogy(VBR.in.SV.T_K-273, eta_total, 'displayname', viscous_fields{ifield})
-end
-legend()
-ylabel(['\eta_{ss} (', VBR.out.viscous.HZK2011.units.eta, ')'])
-xlabel('T (deg C)')
-set(gca, "fontsize", 20)
+
+
+
+
+
+
+
+
+% 6. Looping over structures to compare methods
+% viscous_fields = fieldnames(VBR.out.viscous)  % cell array
+% viscous_fields(1) % single element of cell array
+% viscous_fields{2} % just the string
+
+% nfields = numel(viscous_fields);
+% figure()
+% for ifield = 1:nfields
+%     visc = VBR.out.viscous.(viscous_fields{ifield});
+%     eta_total = visc.eta_total;
+%     hold all
+%     semilogy(VBR.in.SV.T_K-273, eta_total, 'displayname', viscous_fields{ifield})
+% end
+% legend()
+% ylabel(['\eta_{ss} (', VBR.out.viscous.HZK2011.units.eta, ')'])
+% xlabel('T (deg C)')
+% set(gca, "fontsize", 20)
 
 % 7. saving results
-help VBR_save
-VBR_save(VBR, 'myvbr_results.mat')  % alias to save(...)
-VBR = load('myvbr_results.mat');
-VBR_save(VBR, 'myvbr_results.mat', 1) % exclude the state variable arrays
+% help VBR_save
+% VBR_save(VBR, 'myvbr_results.mat')  % alias to save(...)
+% VBR = load('myvbr_results.mat');
+% VBR_save(VBR, 'myvbr_results.mat', 1) % exclude the state variable arrays
 
